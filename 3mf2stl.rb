09b0872f0cd274d39207b7aa606d454cf4ecd8fb
Aidan Gibson <tronicdude@gmail.com>
2024-03-20 00:18:24 -0700

    init

--- 3mf2stl.rb
+++ 3mf2stl.rb
@@ -1,4 +1,4 @@
-class Mf2stl < Formula
+class 3mf2stl < Formula
   desc "Command-line utility to convert 3MF files to STL format"
   homepage "https://github.com/lemgandi/3mf2stl"
   url "https://github.com/lemgandi/3mf2stl.git", branch: "master"
