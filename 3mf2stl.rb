5bf468ccb35dbbc5b7dfc653b38b909fda8e1bef
Aidan Gibson <tronicdude@gmail.com>
2024-03-20 00:17:30 -0700

    init

--- 3mf2stl.rb
+++ 3mf2stl.rb
@@ -1,11 +1,8 @@
-cask "3mf2stl" do
-  version "1.0.0"
-  sha256 :no_check
-
-  url "https://github.com/lemgandi/3mf2stl.git"
-  name "3mf2stl"
+class Mf2stl < Formula
   desc "Command-line utility to convert 3MF files to STL format"
   homepage "https://github.com/lemgandi/3mf2stl"
+  url "https://github.com/lemgandi/3mf2stl.git", branch: "master"
+  version "1.0.0"
   license "GPL-3.0-only"
 
   depends_on "libzip"
